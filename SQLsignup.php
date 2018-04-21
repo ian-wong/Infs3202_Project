85fba84bd5945371ce65cbabfb4277f9a1e949ba
Anthony Hanh <anthonysailou3@gmail.com>
2018-04-21 14:18:26 +1000

    asdf

--- SQLsignup.php
+++ SQLsignup.php
@@ -1,3 +1,13 @@
+<html>
+    <head>
+
+        
+    </head>
+    <body>
+        <h1>sqlsignup.php</h1>
+    
+
+
 <?php
 include 'connectMySQL.php';
 include 'signup.php';
@@ -12,6 +22,18 @@ if (isset($_POST['fNameInput']) && !empty($_POST['lNameInput']) && !empty($_POST
     $lName = $_POST['lNameInput'];
     $email = $_POST['emailInputReg'];
     $password = $_POST['passwordInput'];
+
+    //Checking Safe MySQL Data Insertion
+    $fName = mysqli_real_escape_string($db->link, $_POST["fNameInput"]);
+    $lName = mysqli_real_escape_string($db->link, $_POST["lNameInput"]);
+    $email = mysqli_real_escape_string($db->link, $_POST["emailInputReg"]);
+    $password = mysqli_real_escape_string($db->link, $_POST["passwordInput"]);
+    
+    $insertuser = "INSERT INTO user(firstname, surname, email, password) VALUES ('$fName', '$lName', '$email', '$password')";
+    mysql_query($insertuser); //mysql_query($insertuser, $db->link);
+    
+    //for auto increment id
+    $id = mysqli_insert_id($db->link);
 }
 
 /*
@@ -27,3 +49,5 @@ if(!$result){
 }
 */
 ?>
+</body>
+</html>
