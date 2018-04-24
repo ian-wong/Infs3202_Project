c85714b9e4d3f61f043d3b91bfa4d0bfc7af38fb
Anthony Hanh <anthonysailou3@gmail.com>
2018-04-24 18:43:05 +1000

    24/04 search function - photo

--- SQLgetphoto.php
+++ SQLgetphoto.php
@@ -2,8 +2,6 @@
     include('connectMySQL.php');
 
     $id = $_GET['id'];
-    // do some validation here to ensure id is safe
-
 
     $sqlselphoto= "SELECT photos FROM accommodation WHERE aid=$id";
     $result = mysqli_query($conn, $sqlselphoto);
