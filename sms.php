00c2dc2378785b4c6c72c6b0e7da522a575b59f9
Anthony Hanh <anthonysailou3@gmail.com>
2018-05-19 17:30:43 +1000

    19/05 Don't execute sms.php

---
+++ sms.php
@@ -0,0 +1,44 @@
+<?php
+    function sendSMS($content) {
+        $ch = curl_init('https://api.smsbroadcast.com.au/api-adv.php');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec ($ch);
+        curl_close ($ch);
+        return $output;    
+    }
+
+    $username = 'sailou';
+    $password = 'anto333anto';
+    //$destination = '0400000000'; //Multiple numbers can be entered, separated by a comma
+    $destination = '0402199177'; //Ian's
+    $source    = 'Quest Hotel';
+    //$text = 'This is our test message from Quest Hotel, tell me if this works please Thanks.';
+    $text = 'Thank you for using Quest Hotel ';
+    //$ref = 'abc123';
+    $ref = '';
+    
+    $content =  'username='.rawurlencode($username).
+                '&password='.rawurlencode($password).
+                '&to='.rawurlencode($destination).
+                '&from='.rawurlencode($source).
+                '&message='.rawurlencode($text).
+                '&ref='.rawurlencode($ref);
+  
+    $smsbroadcast_response = sendSMS($content);
+    $response_lines = explode("\n", $smsbroadcast_response);
+    
+     foreach( $response_lines as $data_line){
+        $message_data = "";
+        $message_data = explode(':',$data_line);
+        if($message_data[0] == "OK"){
+            echo "The message to ".$message_data[1]." was successful, with reference ".$message_data[2]."\n";
+        }elseif( $message_data[0] == "BAD" ){
+            echo "The message to ".$message_data[1]." was NOT successful. Reason: ".$message_data[2]."\n";
+        }elseif( $message_data[0] == "ERROR" ){
+            echo "There was an error with this request. Reason: ".$message_data[1]."\n";
+        }
+    }
+    
+?>
