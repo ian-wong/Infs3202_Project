4f9b6e4d1babcf75898ffd1da515d9ce0c5c9e2f
Ian Wong <ianwongy23@gmail.com>
2018-05-25 16:01:27 +1000

    added recaptcha back

--- signup.php
+++ signup.php
@@ -97,7 +97,9 @@
                     <input type="password" class="form-control" id="confirmPassInput" name="confirmPassInput">
                 </div>
             </div>
-
+            <div class="form-group">
+                <div class="g-recaptcha" data-sitekey="6LfiWlsUAAAAABYaGDk6bi3Z5tW1I60dbdt1PzPh"></div>
+            </div>
 
             <div class="form-group">
                 <div class="col-md-7">
